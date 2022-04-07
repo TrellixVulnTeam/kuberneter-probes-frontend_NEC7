ce16d43b649e66a495908afe8a14708122e2a4bc
mmartofel <mmartofe@redhat.com>
2022-04-07 13:20:12 +0200

    add env settings 2

--- deployment/environment.ts
+++ deployment/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   backendURL:  'http://probes-project01.apps.ocp4-v2x7r-ipi.azure.opentlc.com',
-  userListURL: 'http://probes-project01.apps.ocp4-v2x7r-ipi.azure.opentlc.com/api/person/list',
+  userListURL: 'http://probes-project01.apps.ocp4-v2x7r-ipi.azure.opentlc.com/api/person/all',
   userAddURL:  'http://probes-project01.apps.ocp4-v2x7r-ipi.azure.opentlc.com/api/person/add',
   addedFromConfigMap: 'yes'
 };
